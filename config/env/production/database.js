050e781110ee0e81a80aac34633bd25a7ce78d12
DanielStefan11 <dannielstefan592@gmail.com>
2023-03-20 17:38:53 +0200

    prepare strapi app for deployment on digital ocean

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,16 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
+// ./config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: false,
+        ca: env('DATABASE_CA')
       },
     },
     debug: false,
